1ac61018e766bc758870937d0ad6d688e70e2c6a
Ben <ishmandoo@gmail.com>
2016-10-06 09:09:08 -0400

    added replaceTile and replace

--- map.hs
+++ map.hs
@@ -1,6 +1,6 @@
-main = print $ replace myMap 1 1 Path
+main = print $ replaceTile myMap 1 1 Forest
   where
-    myMap = Map [[Blank, Forest, Blank, Blank],[Blank, Blank, Blank, Blank],[Blank, Blank, Blank, Blank],[Blank, Blank, Blank, Blank]]
+    myMap = Map [[Blank, Blank, Blank, Blank],[Blank, Blank, Blank, Blank],[Blank, Blank, Blank, Blank],[Blank, Blank, Blank, Blank]]
 
 data Map = Map [[Tile]]
 
@@ -26,8 +26,17 @@ makePath (Map tiles) x y = (Map tiles)
 --replace (Map (a:as)) x y new = a:(replace (Map as) x y new)
 
 
-replace :: Map -> Int -> Int -> Tile -> Map
-replace (Map tiles) x y tile = (fst split_rows) ++ ((fst split_row) ++ [tile] ++ (tail (snd split_row))) ++ (tail (snd split_rows))
-  where
-    split_row = splitAt x (head (snd split_rows))
-    split_rows = splitAt y tiles
+--replace :: Map -> Int -> Int -> Tile -> Map
+--replace (Map tiles) x y tile = Map ((fst split_rows) ++ ((fst split_row) ++ [tile] ++ (tail (snd split_row))) ++ (tail (snd split_rows)))
+--  where
+--    split_row = splitAt x (head (snd split_rows))
+--    split_rows = splitAt y tiles
+
+replace :: [a] -> Int -> a -> [a]
+replace [] _ _ = []
+replace (a:as) i new
+  | i == (length as) = new:as
+  | otherwise = a:(replace as i new)
+
+replaceTile :: Map -> Int -> Int -> Tile -> Map
+replaceTile (Map tiles) x y tile = Map (replace tiles y (replace (tiles!!x) x tile))
